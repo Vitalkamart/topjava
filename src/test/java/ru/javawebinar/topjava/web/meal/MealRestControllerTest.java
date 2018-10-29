af39cfc71f789aa99990abb803109550748b1975
Mart <vitalkamart@gmail.com>
2018-10-29 22:23:23 +0300

    hw3 prepare

---
+++ src/test/java/ru/javawebinar/topjava/web/meal/MealRestControllerTest.java
@@ -0,0 +1,32 @@
+package ru.javawebinar.topjava.web.meal;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MealRestControllerTest {
+
+    @Test
+    public void get() {
+    }
+
+    @Test
+    public void delete() {
+    }
+
+    @Test
+    public void getAll() {
+    }
+
+    @Test
+    public void create() {
+    }
+
+    @Test
+    public void update() {
+    }
+
+    @Test
+    public void getBetween() {
+    }
+}
